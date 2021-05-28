8e2a7bdef36ac3af5df20d7d9438ae021998d95c
arpitkarnatak <arpitkarnatak47@gmail.com>
2021-05-29 00:20:39 +0530

    Bubble Sort Added with Visualization

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const [SliderValue, setSliderValue] = useState(1)
   const [Array, setArray] = useState([])
-
+  const [Width, setWidth] = useState(800)
 
    function generateRandomArray(nums) {
     let test_array = []
@@ -18,28 +18,91 @@ function App() {
 
   useEffect(() => {
     setArray(generateRandomArray(SliderValue))
+    setWidth((800-5*(SliderValue))/SliderValue)
+    for(let i=0;i<Array.length;i++){
+      document.getElementById('block-'+i.toString()).style.backgroundColor = '#2176ff'
+    }
   }, [SliderValue])
 
+
+  function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+
+  async function BubbleSort(arr){
+    for(var i = 0; i < arr.length -1; i++){
+      for(var j = i+1; j < arr.length; j++){
+        var p = document.getElementById('block-'+i.toString())
+        var q = document.getElementById('block-'+j.toString())
+        p.style.backgroundColor = '#f1c40f'
+        q.style.backgroundColor = '#f1c40f'
+        await sleep(50)
+
+        if (p.style.height > q.style.height){
+          p.style.backgroundColor = '#f4989c'
+          q.style.backgroundColor = '#f4989c'
+          var temp = p.style.height;
+          p.style.height = q.style.height;
+          q.style.height  = temp
+          await sleep(50)
+        }
+
+        p.style.backgroundColor = '#2176ff'
+        q.style.backgroundColor = '#2176ff'
+        await sleep(50)
+      }
+    }
+    for(i=0;i<arr.length;i++){
+      p = document.getElementById('block-'+i.toString())
+      p.style.backgroundColor = '#32e875'
+      await sleep(50)
+    }
+    setArray(Array.sort())
+  }
+  
+/*
+function quickSort(origArray) {
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) {
+			if (origArray[i] <= pivot) {
+				left.push(origArray[i]);
+			} else {
+				right.push(origArray[i]);
+			}
+		}
+
+		return newArray.concat(quickSort(left), pivot, quickSort(right));
+	}
+}
+*/
 
 
   return (
     <div className="App">
-      <input type="range" min="1" max="80" value={SliderValue} onChange={(e)=>setSliderValue(e.target.value)} class="slider" id="myRange"></input>
-      {SliderValue}
+      <input className='slider-length' type="range" min="1" max="80" value={SliderValue} onChange={(e)=>setSliderValue(e.target.value)} class="slider" id="myRange"></input>
+      <button onClick={()=>BubbleSort(Array)}>Bubble Sort</button>
+      {/*<button onClick={()=>quickSort(Array)}>Quick Sort</button> */}
+
+
       <div className= 'outer-div'>
           <div className="blocks-div">
-
-
-            {SliderValue<28 ? Array.map((i)=>
-            <div className='block' style={{"height":i, width: (800-5*(SliderValue))/SliderValue}} id={"block-"+i.toString()}>{i}</div>) :
-            Array.map((i)=>
-            <div className='block' style={{"height":i, width: (800-5*(SliderValue))/SliderValue}} id={"block-"+i.toString()}></div>)}
-
-
-            
-
+            {Array.map((i, index)=>
+            <div className='block' style={{"height":i, width: Width}} id={"block-"+index.toString()}></div>)}
           </div>
       </div>
+
+
     </div>
   );
 }
