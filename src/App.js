d7701ae453940e63c8b57235a134b7741a518aad
arpitkarnatak <arpitkarnatak47@gmail.com>
2021-05-28 19:18:35 +0530

    Bars and slider added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+
+
+  const [SliderValue, setSliderValue] = useState(1)
+  const [Array, setArray] = useState([])
+
+
+   function generateRandomArray(nums) {
+    let test_array = []
+    for(let i=0;i<nums;i++){
+      test_array = [...test_array, Math.floor(Math.random() * (300 - 20 + 1) + 100)]
+    }
+    return test_array
+  }
+
+  
+
+  var indents = (num) =>{
+    let arr = []
+    let x = generateRandomArray(num)
+    if (num < 40){
+      for(let i=0; i< num; i++){
+        arr = [...arr,<div className='block' style={{"height":x[i], width: (800-5*(num))/num}} id={"block-"+i.toString()}>{x[i]}</div>]
+      }
+    }else{
+      for(let i=0; i< num; i++){
+        arr = [...arr,<div className='block' style={{"height":x[i], width: (800-5*(num))/num}} id={"block-"+i.toString()}>{}</div>]
+      }
+    }
+    return arr
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="range" min="1" max="80" value={SliderValue} onChange={(e)=>setSliderValue(e.target.value)} class="slider" id="myRange"></input>
+      {SliderValue}
+      <div className= 'outer-div'>
+          <div className="blocks-div">
+            {indents(SliderValue)}
+          </div>
+      </div>
     </div>
   );
 }
